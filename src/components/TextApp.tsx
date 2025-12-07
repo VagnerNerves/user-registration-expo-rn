e214d3e96e6d78f6094e2728db65e123b2707d0b
Vagner Nerves <vagnernervessantos@gmail.com>
2025-12-07 10:01:51 -0300

    feat: Adjusted TextApp to get values theme.

--- src/components/TextApp.tsx
+++ src/components/TextApp.tsx
@@ -1,11 +1,17 @@
 import { Text, type TextProps } from 'react-native'
 
+import { theme } from '../theme/theme'
+
 type TextAppProps = Readonly<TextProps>
 export function TextApp({ children, style, ...rest }: TextAppProps) {
   return (
     <Text
       style={[
-        { fontFamily: 'Roboto_400Regular', fontSize: 14, color: '#0c0a09' },
+        {
+          fontFamily: theme.FONTS.ROBOTO_REGULAR,
+          fontSize: theme.FONTS.SIZE.BASE,
+          color: theme.COLORS.STONE[950]
+        },
         style
       ]}
       {...rest}
