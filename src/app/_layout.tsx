8013be46ef4ac53a4e1e86d92f0cbeed354ee2b6
Vagner Nerves <vagnernervessantos@gmail.com>
2025-12-07 08:42:34 -0300

    feat: Install font Roboto and config.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,39 +1,38 @@
 import 'react-native-reanimated'
 
-import { useColorScheme } from 'react-native'
+import { useEffect } from 'react'
 
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
 
-import { Stack } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
 
-// SplashScreen.preventAutoHideAsync();
+import { StatusBar } from 'expo-status-bar'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
 
-  // const [loaded, error] = useFonts({
-  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  //   ...FontAwesome.font,
-  // });
-
-  // // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  // useEffect(() => {
-  //   if (error) throw error;
-  // }, [error]);
-
-  // useEffect(() => {
-  //   if (loaded) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded]);
-
-  // if (!loaded) {
-  //   return null;
-  // }
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
 
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="dark" translucent />
+
+      {fontsLoaded ? <Stack screenOptions={{ headerShown: false }} /> : null}
     </ThemeProvider>
   )
 }
