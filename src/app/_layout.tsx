3bafba75ac3205ce3d38e0341e21c5712c35cb16
Vagner Nerves <vagnernervessantos@gmail.com>
2025-12-07 08:09:28 -0300

    feat: Initial project.

---
+++ src/app/_layout.tsx
@@ -0,0 +1,39 @@
+import "react-native-reanimated";
+
+import { useColorScheme } from "react-native";
+
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+
+import { Stack } from "expo-router";
+
+// SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  // const [loaded, error] = useFonts({
+  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  //   ...FontAwesome.font,
+  // });
+
+  // // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  // useEffect(() => {
+  //   if (error) throw error;
+  // }, [error]);
+
+  // useEffect(() => {
+  //   if (loaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
+}
