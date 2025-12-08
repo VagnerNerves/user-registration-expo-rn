d7c440802e48ad0df73f769acfabb6c6e3024590
Vagner Nerves <vagnernervessantos@gmail.com>
2025-12-07 21:54:37 -0300

    feat: Create use Keyboard Status to verify keyboard open.

---
+++ src/hooks/useKeyboardStatus.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Keyboard, Platform } from 'react-native'
+
+export function useKeyboardStatus() {
+  const [keyboardOpen, setKeyboardOpen] = useState(false)
+
+  useEffect(() => {
+    const showEvent =
+      Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow'
+    const hideEvent =
+      Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide'
+
+    const showSubscription = Keyboard.addListener(showEvent, () =>
+      setKeyboardOpen(true)
+    )
+    const hideSubscription = Keyboard.addListener(hideEvent, () =>
+      setKeyboardOpen(false)
+    )
+
+    return () => {
+      showSubscription.remove()
+      hideSubscription.remove()
+    }
+  }, [])
+
+  return { keyboardOpen }
+}
